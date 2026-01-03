795e41ddcb35c8b7330e20d880775cc314c23a50
enji-coder <enji.20.patel@gmail.com>
2026-01-03 11:43:01 +0530

    new content added

---
+++ JAVASCRIPT/Array_example.js
@@ -0,0 +1,28 @@
+/*
+Array in js which is represent by [] braces 
+
+array which is contain similar and dis-similar data elements 
+syntax : 
+        var arr = [];
+*/
+arr = [] 
+console.log(arr);
+
+arr = [10,20,"python","java"]
+
+console.log(arr);
+
+// find length of array 
+
+console.log(arr.length);
+
+arr.push("FLUTTER")  // it will add value at end of the array 
+
+console.log(arr);
+
+arr.unshift("DATA SCIENCE");
+
+console.log(arr);
+
+
+
---
+++ JAVASCRIPT/Array_example2.js
@@ -0,0 +1,19 @@
+a1 = ["C","C++","java","Python","Flutter"]
+
+console.log(a1);
+
+for(var index in a1)
+{
+    console.log(a1[index]);
+}
+console.log("\n");
+
+a1.forEach((sub)=>{
+    console.log(sub);  
+})
+console.log("\n");
+
+for(var i=0;i<a1.length;i++)
+{
+    console.log(a1[i]);
+}
